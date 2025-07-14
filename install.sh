05f35206bfaa7cf66ed1d303b69e7e2b72234c88
Neko-Kuroi <88179359+Neko-Kuroi@users.noreply.github.com>
2025-07-14 17:39:41 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Starting dotfiles setup..."
+
+# ここに、後で追加する設定ファイルのシンボリックリンク作成などを記述します
+# 例: link_file "$HOME/dotfiles/.bashrc" "$HOME/.bashrc"
+
+echo "Dotfiles setup complete!"
